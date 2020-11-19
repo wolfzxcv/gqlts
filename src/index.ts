e7bb06f14af6e9e7e42ec5b00d3894b11ae92590
Nien-Ying Chou <wolfzxcv@gmail.com>
2020-11-19 17:21:14 +0800

    basic setting

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import { createConnection, getConnectionOptions } from 'typeorm';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { MovieResolver } from './resolvers/MovieResolver';
+
+(async () => {
+  const app = express();
+
+  const options = await getConnectionOptions(
+    process.env.NODE_ENV || 'development'
+  );
+  await createConnection({ ...options, name: 'default' });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [MovieResolver],
+      validate: true
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+  const defaultPort = 8081;
+  const port = process.env.PORT || defaultPort;
+  app.listen(port, () => {
+    console.log(
+      `server started at ${
+        port === defaultPort
+          ? `http://localhost:${defaultPort}/graphql`
+          : process.env.PORT
+      }`
+    );
+  });
+})();
---
+++ src/resolvers/MovieResolver.ts
@@ -0,0 +1,57 @@
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  Int,
+  Query,
+  InputType,
+  Field
+} from 'type-graphql';
+import { Movie } from '../../src/entity/Movie';
+
+@InputType()
+class MovieInput {
+  @Field(() => String)
+  title: string;
+
+  @Field(() => Int)
+  minutes: number;
+}
+
+@InputType()
+class MovieUpdateInput {
+  @Field(() => String, { nullable: true })
+  title?: string;
+
+  @Field(() => Int, { nullable: true })
+  minutes?: number;
+}
+
+@Resolver()
+export class MovieResolver {
+  @Mutation(() => Movie)
+  async createMovie(@Arg('options', () => MovieInput) options: MovieInput) {
+    const movie = await Movie.create(options).save();
+    return movie;
+  }
+
+  @Mutation(() => String)
+  async updateMovie(
+    @Arg('id', () => Int) id: number,
+    @Arg('input', () => MovieUpdateInput) input: MovieUpdateInput
+  ) {
+    await Movie.update({ id }, input);
+    return `${id} updated successfully`;
+  }
+
+  @Mutation(() => String)
+  async deleteMovie(@Arg('id', () => Int) id: number) {
+    await Movie.delete({ id });
+    return `${id} deleted successfully`;
+  }
+
+  @Query(() => [Movie])
+  movies() {
+    return Movie.find();
+  }
+}
