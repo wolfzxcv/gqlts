bf587c8025fe6ff06388687bc6dd29b5e82f21d8
Nien-Ying Chou <wolfzxcv@gmail.com>
2021-01-23 00:49:31 +0800

    synchronize: false

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { UserResolver } from './resolvers/UserResolver'
   try {
     const options = await getConnectionOptions(process.env.NODE_ENV || 'development')
 
-    await createConnection({ ...options, name: 'default', synchronize: true })
+    await createConnection({ ...options, name: 'default', synchronize: false })
     console.log('database ok')
   } catch (e) {
     console.log(e)
